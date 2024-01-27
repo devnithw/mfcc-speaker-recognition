a3fb4f421f1a0eb5e29576ff39572ef46ca047f8
devnithw <devnithw@gmail.com>
2024-01-27 17:32:30 +0530

    Changed feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,10 +4,13 @@ import numpy as np
 NUM_MFCC = 40
 
 def extract_features_MFCC(audio_file):
-    """Extract MFCC features from an audio file, shape=(TIME, MFCC)."""
+    """
+    Extract MFCC features from an audio file and return numpy array with output shape=(TIME, MFCC).
+    """
+    # Load audio using Librosa
     waveform, sample_rate = librosa.load(audio_file, sr=None)
 
-    # Convert to mono channel
+    # Check whether audio is mono channel otherwise convert to mono
     if len(waveform.shape) == 2:
         waveform = librosa.to_mono(waveform.transpose())
 
@@ -15,8 +18,7 @@ def extract_features_MFCC(audio_file):
     if sample_rate != 16000:
         waveform = librosa.resample(waveform, sample_rate, 16000)
 
-    # Mel-frequency cepstral coefficients (MFCCs) are robust to noise bcoz of logarithmic compression
+    # Get Mel Frequency Cepstral Coefficients
     features = librosa.feature.mfcc(y=waveform, sr=sample_rate, n_mfcc=NUM_MFCC)
-    # the shape of features will be 40 X 441, where 40 represent featues where as 441 represent frames
 
     return features.transpose()
