4212954372b7dfa16ce328a95807bd14d4ed6273
devnithw <devnithw@gmail.com>
2024-01-27 23:52:34 +0530

    Added load data

--- load_data.py
+++ load_data.py
@@ -1,25 +1,36 @@
 import os
 import glob
+from torch.utils.data import Dataset, DataLoader, Subset
+import feature_extraction
+import numpy as np
 
-def get_speaker_to_utterance(data_dir):
-    """
-    Returns a dictionary with speaker_id(keys) mapping to lists with utterance file path(values)
-    data_sir - folder containing train data
-    """
+class TripletDataset(Dataset):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.files = sorted(os.listdir(folder_path))
+        self.speakers = sorted(set(record.split('-')[0] for record in self.files))
+        self.data = self.load_data()
 
-    speaker_to_utterance = dict()
+    def load_data(self):
+        data = []
+        for recording in self.files:
+            path = os.path.join(self.folder_path, recording)
+            label = recording.split('-')[0]
+            data.append((feature_extraction.get_features(path), label))
+        return data
 
-    # Get all the files in data folder 
-    files_list = glob.glob(os.path.join(data_dir, "*"))
+    def __len__(self):
+        return len(self.data)
 
-    for file in files_list:
-        # Get speaker's id from the file name (eg: a single train data file is named "spk_2-2_1_1_0_d5_ch5.wav")
-        speaker_id = os.path.basename(file)
-        speaker_id = speaker_id.split("-")[0]
+    def __getitem__(self, index):
+        anchor = self.data[index]
 
-        # Add file path to list
-        if speaker_id not in speaker_to_utterance:
-            speaker_to_utterance[speaker_id] = []
-        speaker_to_utterance[speaker_id].append(file)
+        # Sample positive from the same class
+        positive_class_samples = [i for i, (features, label) in enumerate(self.data) if label == anchor[1]]
+        positive_index = np.random.choice(positive_class_samples)
 
-    return speaker_to_utterance
+        # Sample negative from a different class
+        negative_class_samples = [i for i, (features, label) in enumerate(self.data) if label != anchor[1]]
+        negative_index = np.random.choice(negative_class_samples)
+
+        return index, positive_index, negative_index
